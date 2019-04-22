aecf8df7f6ca31703c6e279df8eb2aaa1ac21793
Kiran Korey <kkorey@uncc.edu>
2019-04-21 21:48:48 -0400

    Bug Fixes - Admin activity and End reading

--- app/src/main/java/com/uncc/mileagetracker/Models/Ride.kt
+++ app/src/main/java/com/uncc/mileagetracker/Models/Ride.kt
@@ -1,12 +1,11 @@
 package com.uncc.mileagetracker.Models
 
-class Ride {
-    var id: String? = null
-    var startTime: String? = null
-    var endtTime: String? = null
-    var startReading: Int? = null
-    var endReading: Int? = null
+data class Ride(
+    var id: String? = null,
+    var startTime: String? = null,
+    var endtTime: String? = null,
+    var startReading: Int? = null,
+    var endReading: Int? = null,
     var total: Int? = null
+)
 
-    constructor(){}
-}
